839401af9c78063de8cf659997889e4b0374bd5c
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-08-17 21:12:43 +0200

    Behind the project!
    * Added a behind the project section, where visitors can see some of my lessons and the tech-stack for the project.

--- client/src/components/MiscComponents/DropDownCard.tsx
+++ client/src/components/MiscComponents/DropDownCard.tsx
@@ -4,13 +4,14 @@ import {useNavigate} from "react-router-dom";
 
 
 type DropDownCardProps = {
-    list: any
+    list?: any
     title: string
     singleElement?: React.ReactNode
+    arrow?: boolean
 }
 
 
-export const DropDownCard = ({list, title, singleElement}:DropDownCardProps) => {
+export const DropDownCard = ({list, title, arrow, singleElement}:DropDownCardProps) => {
 
     const [isOpen, setIsOpen] = useState(false);
     const [numberOfExercises, setNumberOfExercises] = useState<number>(list?.length || []);
@@ -20,13 +21,22 @@ export const DropDownCard = ({list, title, singleElement}:DropDownCardProps) => 
     return (
         <div className="card-container">
 
-            <h3 onClick={toggleDropdown} className="dropdown-title">
-                {title}
 
-                {list && (
-                    <span>({numberOfExercises})</span>
+            <div className={'drop-down-header justify-between flex flex-row gap-5'}>
+
+                <h3 onClick={toggleDropdown} className="dropdown-title flex gap-1">
+                    {title}
+                    {list && (
+                        <span>({numberOfExercises})</span>
+                    )}
+                </h3>
+
+                {arrow && (
+                    <svg className={`drop-down-icon ${isOpen ? 'rotate-icon' : ''}`} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m280-400 200-200 200 200H280Z"/></svg>
                 )}
-            </h3>
+
+            </div>
+
 
             <div className={`sub-menu ${isOpen ? 'show' : ''}`}>
                 <div className="sub-menu-inner">
